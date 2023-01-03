e5caaa462258ce7ed0f70ec062bf6d4ed0298e5b
Akhil Chandran <119779525+Millionairebhai778@users.noreply.github.com>
2023-01-03 13:08:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/WebX-Divin/File-Sharing-Bot'>Click here</a>\n○ Channel : @FILMWORLDOFFI\n○ Support Group : @FILMWORLDOFFI \n○ Credits : @FILMWORLDOFFICIA</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/kurachkanjiedukkatte/8'>Click here</a>\n○ Channel : @FILMWORLDOFFI\n○ Support Group : @FILMWORLDOFFI \n○ Credits : @FILMWORLDOFFICIA</b>",
             disable_web_page_preview = False,
             reply_markup = InlineKeyboardMarkup(
                 [
