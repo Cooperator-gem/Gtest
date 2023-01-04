78be1ecf283258fceb91b810168c59989939870e
Akhil chandran <97656070+Cooperator-gem@users.noreply.github.com>
2023-01-04 18:06:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,22 +13,22 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5905642028:AAEtnTMw7P0A33gsqqbvN0mLbgdVc9Qkxtk")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "gh"))
+APP_ID = int(os.environ.get("APP_ID", "1377849"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "gh")
+API_HASH = os.environ.get("API_HASH", "2aba0868b0b9da9c4dd9ad656487078c")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "gh"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001859828141"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "gh"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "611969501"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "gh")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://bot1:bot1@cluster0.cuyh4kk.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
